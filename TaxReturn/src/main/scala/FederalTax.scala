b708ea4869e1e5c5b116c3be24f84307bc47e438
dvdarpan <dvdarpan@gmail.com>
2016-01-30 21:41:04 -0600

    dd

--- TaxReturn/src/main/scala/FederalTax.scala
+++
@@ -1,8 +0,0 @@
-
-object FederalTax {
-
-  val rate = 3 / 100D
-  val daily = 365D
-  val interestCal = Formulas.compoundInterest(_: Double, daily, rate, _: Double)
-  def interestCalDaily(p:Double ,t:Double) = {interestCal(p,t/daily) } 
-  }
